c91061ce0977ce2e6de9575a79350693113140e3
Peter Dunshee <peter@petezah.com>
2017-02-23 09:02:02 -0600

    Handle all REST methods Also return result for test purposes

--- ScfServer/server.py
+++ ScfServer/server.py
@@ -4,14 +4,38 @@
 #
 
 import http.server
+from json.encoder import JSONEncoder
 
 class FacadeRequestHandler(http.server.BaseHTTPRequestHandler):
     """Request handler for the cloud facade HTTP server"""
 
     def do_GET(self):
         """HTTP GET handler"""
+        self.handle_request("GET")
 
-        self.wfile.write(bytes("<html><body>This is a test</body></html>", "UTF-8"))
+    def do_POST(self):
+        """HTTP POST handler"""
+        self.handle_request("POST")
+
+    def do_PUT(self):
+        """HTTP PUT handler"""
+        self.handle_request("PUT")
+
+    def do_DELETE(self):
+        """HTTP DELETE handler"""
+        self.handle_request("DELETE")
+
+    def handle_request(self, method):
+        result = {
+            "method":method,
+            "path":self.path
+        }
+        if "Content-Length" in self.headers:
+            result["body"] = self.rfile.read(int(self.headers["Content-Length"])).decode("UTF-8")
+        
+        encoder = JSONEncoder()
+        content = encoder.encode(result)
+        self.wfile.write(bytes(content, "UTF-8"))
         self.send_response(200)
 
 
